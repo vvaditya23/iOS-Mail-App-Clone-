37f95b04f83d28722360a3ea3aa6e6c5ad0cc272
Aditya Vyavahare <vvaditya23@icloud.com>
2023-11-04 10:24:03 +0530

    Initial Commit

---
+++ iOS Mail App (Clone)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Mail App (Clone)
+//
+//  Created by ヴィヤヴャハレ・アディティヤ on 04/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
