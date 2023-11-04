e865760aba51647eb7c9c0cff0e78155673a592f
Aditya Vyavahare <vvaditya23@icloud.com>
2023-11-04 14:23:36 +0530

    added animaton to move list to right side when in editing mode.

--- iOS Mail App (Clone)/ViewController.swift
+++ iOS Mail App (Clone)/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
     var mailListCollectionView: UICollectionView!
+    
+    //constraint to shift the list to right side while in editing mode
+    private var collectionViewLeadingConstraint: NSLayoutConstraint?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Loaded")
-        
+        view.backgroundColor = .systemBackground
 //        // Create a navigation controller with your view controller as the root view controller
         let navigationController = UINavigationController(rootViewController: self)
 
@@ -33,7 +37,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
                 window.rootViewController = navigationController
             }
     }
-//    
+//
     override func setEditing(_ editing: Bool, animated: Bool) {
         super.setEditing(editing, animated: animated)
         
@@ -42,6 +46,9 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         if editing {
             // Enable multi-selection, show checkboxes, etc.
             navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelEditing))
+            
+            //shift the list to right
+            collectionViewLeadingConstraint?.constant = 50
             
             // Show the checkboxes in each cell
                     for indexPath in mailListCollectionView.indexPathsForVisibleItems {
@@ -53,6 +60,9 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             // Disable multi-selection, hide checkboxes, etc.
             navigationItem.rightBarButtonItem = editButtonItem
             
+            //bring the list back to original position
+            collectionViewLeadingConstraint?.constant = 0
+            
             // Hide the checkboxes in each cell
                     for indexPath in mailListCollectionView.indexPathsForVisibleItems {
                         if let cell = mailListCollectionView.cellForItem(at: indexPath) as? MailListCell {
@@ -60,6 +70,11 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
                         }
                     }
         }
+        
+        // You may need to call layoutIfNeeded to apply the constraint changes
+            UIView.animate(withDuration: 0.3) {
+                self.view.layoutIfNeeded()
+            }
     }
     @objc func cancelEditing() {
         // Implement any logic needed when the user cancels the editing mode.
@@ -83,10 +98,13 @@ extension ViewController {
         mailListCollectionView.dataSource = self
         view.addSubview(mailListCollectionView)
 
+        collectionViewLeadingConstraint = mailListCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+            collectionViewLeadingConstraint?.isActive = true
+        
         mailListCollectionView.translatesAutoresizingMaskIntoConstraints = false
                 NSLayoutConstraint.activate([
                     mailListCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-                    mailListCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//                    mailListCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
                     mailListCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
                     mailListCollectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
                 ])
@@ -118,7 +136,7 @@ extension ViewController {
 //        mailListCollectionView.register(MailListCell.self, forCellWithReuseIdentifier: "CellIdentifier")
 //
 //        view.addSubview(mailListCollectionView)
-//        
+//
 //        // Set constraints to pin the collection view to the edges of the view controller's view
 //        mailListCollectionView.translatesAutoresizingMaskIntoConstraints = false
 //        NSLayoutConstraint.activate([
@@ -135,7 +153,7 @@ extension ViewController {
 //
 //    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 //        let cell = mailListCollectionView.dequeueReusableCell(withReuseIdentifier: "CellIdentifier", for: indexPath) as! MailListCell
-//        
+//
 //        cell.dateLabel.text = "dd/mm/yyyy"
 //        cell.senderLabel.text = "some sender"
 //        cell.subjectLabel.text = "some subject"
